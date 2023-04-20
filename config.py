ff61c95f232665752e5f0bb9c5eaad4d712c434e
ZhuoyangM <zhmeng@ucsd.edu>
2023-04-20 15:13:08 -0700

    some minor changes

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
+    ADMINS = ['2023appli@gmail.com']
     POSTS_PER_PAGE = 3
     LANGUAGES = ['en']
 
